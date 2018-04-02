b13bcbe4b18dc537218424af9b693165d5a37617
CRYDEsigN <Cryden_da@mail.ru>
2018-04-02 11:07:19 +0300

    v 0.1.0

--- index.js
+++ index.js
@@ -15,18 +15,16 @@ const args = require('yargs').argv
 const timestamp = moment().format('YYYYMMDDHHmmss')
 const path = require('path')
 
-var config = {}
-var options = {
+let options = {
   multiFile: false,
+  root: 'config'
+}
+
+let config = {
   environments: {}
 }
 
-let environments = config.environments || {}
-let envId = getEnvId(config)
-let ENVID = envId ? envId.toUpperCase() : undefined
-let environmentTypes = environments.static || keys(config)
-let environmentType = _.includes(environmentTypes, envId) ? envId : environments.default
-config = swapVariables(config)
+let environments = {}
 
 /**
  * Check exist deafult config files
@@ -35,14 +33,14 @@ config = swapVariables(config)
  */
 
 function checkDefaultConfig () {
-  if (fs.existsSync('config.yml') || fs.existsSync('config')) {
+  if (fs.existsSync('config.yml')) {
     return true
   } else {
     return false
   }
 }
 
-console.log('Check default config:', checkDefaultConfig())
+// console.log('Check default config:', checkDefaultConfig())
 
  /**
  * Load yaml file
@@ -62,7 +60,7 @@ function loadYamlFile (file) {
   }
 }
 
-console.log('YAML test:', loadYamlFile('test.yml'))
+// console.log('YAML test:', loadYamlFile('test.yml'))
 
 /**
  * Load Yaml files as Config object
@@ -71,13 +69,22 @@ console.log('YAML test:', loadYamlFile('test.yml'))
  * @returns {obj}
  */
 
-function loadConfig (paths) {
+function loadYamlConfig (paths) {
+  paths = paths || []
+  if (paths === []) {
+    if (checkDefaultConfig()) {
+      paths.push('config.yml')
+    } else {
+      return
+    }
+  }
   let files = []
-
   for (let i = 0; i < paths.length; i++) {
     if (paths[i].endsWith('.yml')) {
-      let keyName = (/([ \w-]+?(?=\.))/.exec(paths[i])[1])
-      files[keyName] = path.join(paths[i])
+      if (fs.existsSync(paths[i])) {
+        let keyName = (/([ \w-]+?(?=\.))/.exec(paths[i])[1])
+        files[keyName] = path.join(paths[i])
+      }
     } else {
       let dirFiles = fs.readdirSync(path.join(paths[i]))
       let dirPath = path.join(paths[i])
@@ -89,12 +96,12 @@ function loadConfig (paths) {
       }
     }
   }
-
-  let config = {}
-
+  if (files === []) {
+    return
+  }
   for (var keyName in files) {
     if (files.hasOwnProperty(keyName)) {
-      if (keyName === 'config') {
+      if (keyName === options.root) {
         config = loadYamlFile(files[keyName])
       } else {
         config[keyName] = loadYamlFile(files[keyName])
@@ -104,34 +111,63 @@ function loadConfig (paths) {
   return config
 }
 
-console.log('Load config:', loadConfig(['temp1', 'temp/config.yml']))
+// loadYamlConfig(['temp1', 'temp/config.yml'])
+// console.log('Load config:', config)
+
+/**
+ * Get EnvId from branch
+ *
+ * @returns {string}
+ */
 
 function getEnvIdFromBranch () {
   try {
     let branch = sh.exec('git status', {
       silent: true
     }).stdout
-
     if (!branch || _.includes(branch, 'fatal:')) {
       return
     }
-
     branch = branch.split('\n')[0]
     branch = branch.replace(/^#?\s?On branch ((\w|-|_|\/|.)+)/, '$1')
-
     if (config.branchRegex) {
       branch = branch.replace(new RegExp(_.trim(config.branchRegex)), '$1')
     }
-
     return _.trimEnd(_.truncate(branch, {
       length: 13,
       omission: ''
     }), '-')
   } catch (e) {
     console.log('ERR: ', e)
-    // Do nothing
   }
 }
+
+// console.log('EnvId from Branch:', getEnvIdFromBranch())
+
+/**
+ * Set config environments
+ *
+ * @param {any} obj
+ * @returns
+ */
+
+function setEnvironments (config) {
+  environments.environments = config.environments || {}
+  environments.envId = getEnvId(config)
+  environments.ENVID = environments.envId ? environments.envId.toUpperCase() : undefined
+  environments.environmentTypes = environments.environments.static || keys(config)
+  environments.environmentType = _.includes(environments.environmentTypes, environments.envId) ? environments.envId : environments.environments.default
+}
+
+// setEnvironments(loadYamlConfig(['temp1', 'temp/config.yml']))
+// console.log('environments:', environments)
+
+/**
+ * Set EnvId
+ *
+ * @param {any} obj
+ * @returns {string}
+ */
 
 function getEnvId (obj) {
   return args.env ||
@@ -144,80 +180,32 @@ function getEnvId (obj) {
     getEnvIdFromBranch()
 }
 
-function substitute (file, p) {
-  let success = false
-  let replaced = p.replace(/\${([\w.-]+)}/g, function (match, term) {
-    if (!success) {
-      success = _.has(file, term)
-    }
-    return _.get(file, term) || match
-  })
-  return {
-    success: success,
-    replace: replaced
-  }
-}
+// console.log('Set EnvID:', getEnvId(loadYamlConfig(['temp1', 'temp/config.yml'])))
 
-function transform (file, obj) {
-  let changed = false
-  let resultant = _.mapValues(obj, function (p) {
-    if (_.isPlainObject(p)) {
-      let transformed = transform(file, p)
-      if (!changed && transformed.changed) {
-        changed = true
-      }
-      return transformed.result
-    }
-    if (_.isString(p)) {
-      let subbed = substitute(file, p)
-      if (!changed && subbed.success) {
-        changed = true
-      }
-      return subbed.replace
-    }
-    if (_.isArray(p)) {
-      for (let i = 0; i < p.length; i++) {
-        if (_.isString(p[i])) {
-          p[i] = substitute(file, p[i]).replace
-        }
-      }
-    }
-    return p
-  })
-  return {
-    changed: changed,
-    result: resultant
-  }
-}
+/**
+ * Print envID
+ *
+ */
 
 function log () {
-  console.log('CONFIG:', envId || '-', environmentType || '-')
+  console.log('CONFIG:', environments.envId || '-', environments.environmentType || '-')
 }
 
 function load (path) {
-  console.log(path)
-  config = loadConfig(path)
+  config = loadYamlConfig(path)
+  setEnvironments(config)
   config = swapVariables(config)
   return config
 }
 
-function requireSettings (settings) {
-  let erredSettings = []
-  settings = _.isString(settings) ? [settings] : settings
-  _.forEach(settings, function (setting) {
-    if (!_.has(config, setting)) {
-      erredSettings.push(setting)
-    }
-  })
+// console.log('CONFIG:', load())
 
-  if (erredSettings.length > 1) {
-    throw new Error('The following settings are required in config.yml: ' + erredSettings.join('; '))
-  }
-
-  if (erredSettings.length === 1) {
-    throw new Error(erredSettings[0] + ' is required in config.yml')
-  }
-}
+/**
+ * Swap config
+ *
+ * @param {any} configFile
+ * @returns {obj}
+ */
 
 function swapVariables (configFile) {
   function readAndSwap (obj) {
@@ -229,22 +217,63 @@ function swapVariables (configFile) {
     } while (altered)
     return obj
   }
-
+  function substitute (file, p) {
+    let success = false
+    let replaced = p.replace(/\${([\w.-]+)}/g, function (match, term) {
+      if (!success) {
+        success = _.has(file, term)
+      }
+      return _.get(file, term) || match
+    })
+    return {
+      success: success,
+      replace: replaced
+    }
+  }
+  function transform (file, obj) {
+    let changed = false
+    let resultant = _.mapValues(obj, function (p) {
+      if (_.isPlainObject(p)) {
+        let transformed = transform(file, p)
+        if (!changed && transformed.changed) {
+          changed = true
+        }
+        return transformed.result
+      }
+      if (_.isString(p)) {
+        let subbed = substitute(file, p)
+        if (!changed && subbed.success) {
+          changed = true
+        }
+        return subbed.replace
+      }
+      if (_.isArray(p)) {
+        for (let i = 0; i < p.length; i++) {
+          if (_.isString(p[i])) {
+            p[i] = substitute(file, p[i]).replace
+          }
+        }
+      }
+      return p
+    })
+    return {
+      changed: changed,
+      result: resultant
+    }
+  }
   let file = options.multiFile ? _.mapValues(configFile, readAndSwap) : configFile
   file = _.merge({},
     file || {},
-    file[environmentType] || {}, {
-      envId: envId,
-      ENVID: ENVID,
+    file[environments.environmentType] || {}, {
+      envId: environments.envId,
+      ENVID: environments.ENVID,
       timestamp: timestamp,
       args
     })
-
   file = readAndSwap(file)
   return file
 }
 
-// module.exports.default = load('config')
-// module.exports.log = log
-// module.exports.require = requireSettings
-// module.exports.load = load
+module.exports.default = load()
+module.exports.log = log
+module.exports.load = load
--- main.js
+++
@@ -1,4 +0,0 @@
-const config = require('./index.js')
-
-// console.log(config)
-console.log(config.load('temp'))
